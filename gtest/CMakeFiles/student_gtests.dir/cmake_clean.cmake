ead9f7825b151ace9ccbc5c193dc1642c3b2cd9c
Chidubem Adinna <adinnac@uci.edu>
2025-03-17 06:18:16 +0000

    making test cases

---
+++ gtest/CMakeFiles/student_gtests.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/student_gtests.dir/gtestmain.cpp.o"
+  "CMakeFiles/student_gtests.dir/gtestmain.cpp.o.d"
+  "CMakeFiles/student_gtests.dir/src/dijkstras.cpp.o"
+  "CMakeFiles/student_gtests.dir/src/dijkstras.cpp.o.d"
+  "CMakeFiles/student_gtests.dir/src/ladder.cpp.o"
+  "CMakeFiles/student_gtests.dir/src/ladder.cpp.o.d"
+  "CMakeFiles/student_gtests.dir/student_gtests.cpp.o"
+  "CMakeFiles/student_gtests.dir/student_gtests.cpp.o.d"
+  "student_gtests"
+  "student_gtests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/student_gtests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,6 +3,16 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Replace, Me) {
-  EXPECT_TRUE(false);
+TEST(ladder, CattoRat) {
+  
+  set<string> word_list;
+
+  load_words(word_list, "../src/words.txt");
+
+  vector<string> e = generate_word_ladder("cat", "rat", word_list);
+  EXPECT_EQ(e.size(), 2);
+  testing::internal::CaptureStdout();
+  print_word_ladder(e);
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_EQ("Word ladder found: cat rat \n", output);
 }
